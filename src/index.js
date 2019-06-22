e9328c6e02ae85feca5a1eccdde825247fd8549c
Alex White <alex.white@daugherty.com>
2019-06-22 17:41:41 -0500

    Work a little mo better

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 const dotenv = require('dotenv');
-const {App} = require('@slack/bolt');
+const {App, LogLevel} = require('@slack/bolt');
 
 dotenv.config();
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  logLevel: LogLevel.DEBUG,
 });
+
+(async () => {
+  const port = process.env.PORT || 3000;
+  const server = await app.start(port);
+  console.log('⚡️ Bolt app is running!', server.address());
+})();
 
 // The echo command simply echoes on command
 app.command('/echo', async ({command, ack, say}) => {
-  console.log("echoing");
   // Acknowledge command request
   ack();
+
+  console.log('echoing');
 
   say(`${command.text}`);
 });
@@ -22,7 +30,3 @@ app.message(':wave:', async ({message, say}) => {
   say(`Hello, <@${message.user}>`);
 });
 
-(async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
-})();
