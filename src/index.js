ceb801b9998e978cf9411c1f2bc8f7a636f492b4
Alex White <alex.white@daugherty.com>
2019-06-22 16:43:02 -0500

    Get nodemon working

--- src/index.js
+++ src/index.js
@@ -4,26 +4,25 @@ const {App} = require('@slack/bolt');
 dotenv.config();
 
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
 });
 
 // The echo command simply echoes on command
-app.command('/echo', async ({ command, ack, say }) => {
-    console.log("echoing");
-    // Acknowledge command request
-    ack();
+app.command('/echo', async ({command, ack, say}) => {
+  console.log("echoing");
+  // Acknowledge command request
+  ack();
 
-    say(`${command.text}`);
+  say(`${command.text}`);
 });
 
-app.message(':wave:', async ({ message, say}) => {
-    console.log("saying hi");
-    say(`Hello, <@${message.user}>`);
+app.message(':wave:', async ({message, say}) => {
+  console.log("saying hi");
+  say(`Hello, <@${message.user}>`);
 });
 
 (async () => {
-    await app.start(process.env.PORT || 3000);
-
-    console.log('⚡️ Bolt app is running!');
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
 })();
