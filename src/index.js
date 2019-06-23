f6895a260ae38b8e60bf30af4b359ac56bdcd742
Aaron Joyce <aaron.joyce@daugherty.com>
2019-06-23 13:51:48 -0500

    update /wrexy-list

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.command('/wrexy-list', async ({command, ack, say}) => {
     ack();
     try {
         const statusResponse = await axios.get(`http://165.22.45.117/api/users/${command.user_id}/statuses`);
-        const statuses = statusResponse.data;
+        const statuses = statusResponse.data.map(status => status.message);
         console.log(`Retrieved statuses: ${statuses.toString()}`);
         say({response_type:'ephemeral' , text:statuses.toString()})
     } catch {
